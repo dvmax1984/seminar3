0aaed10e156cc9dac9e3bd098d8442e32cca081f
Maksim <dvmax1984@mail.ru>
2023-03-02 14:39:34 +0700

    Решили задачу Ex24

--- Ex024/Program.cs
+++ Ex024/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("24. Найти кубы чисел от 1 до N");
+
+int GetNumber (string text) {
+    Console.Write(text);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+int GetResult(int n)
+{
+    int K = 0;
+    for (int i = 1; i < n; i++)
+    {
+        K = i * i * i;
+
+        Console.WriteLine(K.ToString() + "  i= "+ i.ToString());
+    }
+    return K;
+}
+
+
+int n = GetNumber("Введите конечное число: ");
+Console.Write(GetResult(n));
