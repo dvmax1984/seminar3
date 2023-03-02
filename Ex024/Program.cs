9109616d46d4ab7306f9900cb211bb55eae7a12a
Sergei Kamianetskii <i@ksergey.ru>
2023-03-02 10:53:24 +0300

    Поправили метод GetNumber

--- Ex024/Program.cs
+++ Ex024/Program.cs
@@ -1,21 +1,21 @@
 ﻿Console.WriteLine("24. Найти кубы чисел от 1 до N");
 
-int GetNumber (string text) {
-    Console.Write(text);
-    int N = Convert.ToInt32(Console.ReadLine());
-    return N;
+int GetNumber(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
 }
 
 int GetResult(int n)
 {
-    int K = 0;
-    for (int i = 1; i < n; i++)
-    {
-        K = i * i * i;
+  int K = 0;
+  for (int i = 1; i < n; i++)
+  {
+    K = i * i * i;
 
-        Console.WriteLine(K.ToString() + "  i= "+ i.ToString());
-    }
-    return K;
+    Console.WriteLine(K.ToString() + "  i= " + i.ToString());
+  }
+  return K;
 }
 
 
